0f0013a20968b28ee9e1ad211cd8a1831ccbdc49
FGHII <eric.j.klosterman@gmail.com>
2021-03-25 17:01:40 -0500

    completed 5 round game iteration

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function computerPlay() {
   let computerResIndex = Math.floor(Math.random() * 3);
   let computerResponse = options[computerResIndex];
   return computerResponse;
-}
+};
 
-function playerPlay(){
+function playerPlay() {
   let playerResponse = prompt("Enter Rock, Paper, or Scissors:");
   playerResponse = playerResponse.toLowerCase();
   return playerResponse;
-}
+};
 
 function playRound(gameScore) {
   let computerSelection = computerPlay();
@@ -22,29 +22,39 @@ function playRound(gameScore) {
   if ((computerSelection === "paper" && playerSelection === "rock") ||
     (computerSelection === "scissors" && playerSelection === "paper") ||
     (computerSelection === "rock" && playerSelection === "scissors")) {
-      console.log("Computer wins, " + computerSelection + " beats " + playerSelection);
-      gameScore.computerWinCount++;
-      console.log ("Computer: " + gameScore.computerWinCount + " Player: " + gameScore.playerWinCount);
-    } else if ((computerSelection === "scissors" && playerSelection === "rock") ||
-      (computerSelection === "rock" && playerSelection === "paper") ||
-      (computerSelection === "paper" && playerSelection === "scissors")) {
-        console.log("Player wins, "+ gameScore.playerSelection + " beats " + gameScore.computerSelection);
-        gameScore.playerWinCount++;
-        console.log("Computer: " + gameScore.computerWinCount + " Player: " + gameScore.playerWinCount);
-      } else {
-        console.log("It's a draw");
-        console.log("Computer: " + gameScore.computerWinCount + " Player: " + gameScore.playerWinCount);
-      }
-    return gameScore;
-}
+    console.log("Computer wins, " + computerSelection + " beats " + playerSelection);
+    gameScore.computerWinCount++;
+    console.log("Computer: " + gameScore.computerWinCount + " Player: " + gameScore.playerWinCount);
+  } else if ((computerSelection === "scissors" && playerSelection === "rock") ||
+    (computerSelection === "rock" && playerSelection === "paper") ||
+    (computerSelection === "paper" && playerSelection === "scissors")) {
+    console.log("Player wins, " + playerSelection + " beats " + computerSelection);
+    gameScore.playerWinCount++;
+    console.log("Computer: " + gameScore.computerWinCount + " Player: " + gameScore.playerWinCount);
+  } else {
+    console.log("It's a draw");
+    console.log("Computer: " + gameScore.computerWinCount + " Player: " + gameScore.playerWinCount);
+  }
+  return gameScore;
+};
 
 function game() {
   const gameScore = {
     playerWinCount: 0,
     computerWinCount: 0
   };
-  playRound(gameScore);
-  console.log(gameScore);
-}
+  let winFlag = false;
+  while (winFlag == false) {
+    playRound(gameScore);
+    console.log(gameScore);
+    if (gameScore.playerWinCount === 5) {
+      winFlag = true;
+      console.log("The winner of 5 rounds is player!");
+    } else if (gameScore.computerWinCount === 5) {
+      winFlag = true;
+      console.log("The winner of 5 rounds is computer!");
+    }
+  }
+};
 
 game();
